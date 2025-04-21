35d7da536e7c783317a23be9f8c628058417701b
Steve Streza <steve@stevestreza.com>
2025-04-20 23:56:40 -0700

    Fix for logger

--- backend/src/middleware/logger.ts
+++ backend/src/middleware/logger.ts
@@ -1,11 +1,10 @@
 import { Context, Next } from 'koa'
-import { prisma } from '../lib/prisma'
 import { logger } from '../lib/logger'
 
 const requestLogger = logger.child({ context: `request` })
 
 export const logRequest = async (ctx: Context, next: Next) => {
-	logger.debug({ context: ctx }, "Request");
+	requestLogger.debug({ context: ctx }, "Request");
 	await next();
 }
 

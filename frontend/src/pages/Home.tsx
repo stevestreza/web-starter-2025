2cd348e32a807eaf48be824599093e84fdc5407d
Steve Streza <steve@stevestreza.com>
2025-04-20 23:50:13 -0700

    Style

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="space-y-6">
-      <h1 className="text-3xl font-bold text-green">Home Page</h1>
-      <p className="text-nebula-blue-200">
+      <h1 className="text-3xl font-bold text-gray-950">Home Page</h1>
+      <p className="text-gray-800">
 	    Hello world!
       </p>
     </div>
